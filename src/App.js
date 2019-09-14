a8e0676c25b1a9c4b0a333a594783a233eaa3f26
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-09-14 11:22:58 -0500

    Added dependencies to the project. Added Semantic UI. Tested Semantic UI to get a card rendering on the main App page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import { Card, Icon, Image } from 'semantic-ui-react'
+
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Card><h1>My name is Stephen</h1></Card>
       {/* <TabNav /> */}
     </main>
   );
