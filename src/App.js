f7eead98327de11079efcabc0d925c46b350c18f
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-09-16 09:55:20 -0500

    Updating the routing and pathing to render the EpisodeList component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import {CharacterList} from './components/Characters/CharacterList'
@@ -10,6 +10,7 @@ export default function App() {
   return (
     <main>
       <Header />
+      <Route path='/episodes' render={}
       <EpisodeList />
 
       {/* <TabNav /> */}
--- src/components/Episodes/EpisodeCard.js
+++ src/components/Episodes/EpisodeCard.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+    font-size: 62.5%;
+    display: flex;
+    flex-direction: column;
+    width: 17rem;
+    justify-content: space-around;
+    align-items: center;
+    
+    border: .2rem solid white
+    margin: 2rem;
+    border-radius: 1rem;
+    padding: .5rem;
+    background-color: black;
+`;
 
 export const EpisodeCard = (props) => {
-    console.log(props);
-
-    return(
-        <h1> This is a header. </h1>
+    const ep = props.item
+    console.log(ep)
+    return (
+      <Container>
+            <ListHeading>
+                <List>Name: {ep.name}</List>
+                <List>Episode: {ep.episode}</List>
+                <List>Episode: {ep.air_date}</List>
+            </ListHeading>
+      </Container>
     )
 }
